f6e030fd7a4c9b18381a1dd6fc7f3a6210c5ece9
Rudr <rudrprasad@yahoo.com>
2023-11-23 19:05:28 +1200

    pr

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,12 +4,7 @@ import { SignOutContext } from "../../actions/signOut";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
-    const { data: session } = useSession();
-    console.log(session?.user);
-    return (
-        <div>
-            {/* Hero */}
-        </div>
-    );
-
+  // const { data: session } = useSession();
+  // console.log(session?.user);
+  return <div>{/* Hero */}</div>;
 }
